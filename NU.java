0ba7e15718765215a0204fa67b80d8d720f689e7
mihirchakravarti <34199554+mihirchakravarti@users.noreply.github.com>
2017-12-03 14:02:51 -0500

    Create NU.java

---
+++ NU.java
@@ -0,0 +1,10 @@
+//an interface for the normal user, which lists the things a normal user can do
+
+public interface NU {
+
+	public int addEntry();
+	public int EditPhoneBookEntry();
+	public void SortPhoneBookEntry();
+	public int LinearSearchByPhoneNumber();
+	public void PrintUserInfo();
+}
